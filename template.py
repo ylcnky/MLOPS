29c88640c8670479eb268f6b8231f27961c5c23d
Mehmet Yalcinkaya <mehmet.yalcinkaya@novoenergy.se>
2025-01-18 14:24:44 +0100

    fix the folder structure

--- template.py
+++ template.py
@@ -17,7 +17,7 @@ list_of_files = [
     "src/utils/__init__.py",
     "src/utils/utils.py",
     "src/logger/logging.py",
-    "src/exception/exception.py"
+    "src/exception/exception.py",
     "tests/unit/__init__.py",
     "tests/integration/__init__.py",
     "init_setup.sh",
